11551313eb31dc033610eb09a7d73affb146bc22
Dirk Winkel <it@polarwinkel.de>
2017-12-29 14:07:40 +0100

    fix for missing phpldapadmin: added a version here (container is mainly for development purposes anyway)

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-docker rm pc_admin
-docker rm samba
-docker rm ldap
-docker rm php_ldap_admin
-docker rmi philleconnect_ldap
+docker-compose stop
+docker rmi philleconnect_php_ldap_admin
 docker rmi philleconnect_pc_admin
 docker rmi philleconnect_samba
-docker rmi philleconnect_php_ldap_admin
+docker rmi philleconnect_ldap
 docker volume rm philleconnect_philleconnect_admin_config
-docker volume rm philleconnect_philleconnect_ldap_db
 docker volume rm philleconnect_philleconnect_admin_mysql
+docker volume rm philleconnect_philleconnect_ldap_db
