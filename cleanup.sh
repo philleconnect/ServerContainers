c66610a785e1c0b3f5a845d888622a8777277bbd
TheJKM <jkm@jkm-marburg.de>
2018-01-04 15:37:44 +0100

    Correct container / volume names

--- cleanup.sh
+++ cleanup.sh
@@ -4,10 +4,10 @@ docker rm php_ldap_admin
 docker rm pc_admin
 docker rm samba
 docker rm ldap
-docker rmi philleconnect_php_ldap_admin
-docker rmi philleconnect_pc_admin
-docker rmi philleconnect_samba
-docker rmi philleconnect_ldap
-docker volume rm philleconnect_philleconnect_admin_config
-docker volume rm philleconnect_philleconnect_admin_mysql
-docker volume rm philleconnect_philleconnect_ldap_db
+docker rmi servercontainers_php_ldap_admin
+docker rmi servercontainers_pc_admin
+docker rmi servercontainers_samba
+docker rmi servercontainers_ldap
+docker volume rm servercontainers_philleconnect_admin_config
+docker volume rm servercontainers_philleconnect_admin_mysql
+docker volume rm servercontainers_philleconnect_ldap_db
