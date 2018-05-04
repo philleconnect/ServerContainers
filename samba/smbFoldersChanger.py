89e03cc49557ac320849602e54715247fec47d15
Dirk Winkel <it@polarwinkel.de>
2018-05-04 15:51:40 +0200

    fix: lost group folders on container rebuild/restart

--- samba/smbFoldersChanger.py
+++ samba/smbFoldersChanger.py
@@ -10,7 +10,28 @@ import json
 import os
 
 conffile = '/etc/samba/smb.conf'
+bakfile= '/home/FolderConf.txt'
 
+def includeBackup():
+    # delete old stuff:
+    old = open(conffile, 'r')
+    lines = old.readlines()
+    old.close()
+    new = open(conffile, 'w')
+    found = False
+    for line in lines:
+        if line == "# begin ExchangeFolders; DON'T CHANGE MANUALLY!\n":
+            found = True
+        if not found:
+            new.write(line)
+        if line == "# end ExchangeFolders; DON'T CHANGE MANUALLY!\n":
+            found = False
+    # write backup file to config:
+    newconf = open(bakfile, 'r')
+    new.writelines(newconf)
+    newconf.close()
+    new.close()
+    os.system('service smbd reload')
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     ''' Handler to apply samba-config-changes that are received as POST-data '''
@@ -35,12 +56,38 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
     def configSamba(self, data):
         ''' edit the samba config and restart it if changed '''
+        # create new config block:
+        newconf = ['']
+        newconf.append("# begin ExchangeFolders; DON'T CHANGE MANUALLY!\n")
+        j = json.loads(data)
+        for d in j:
+            newconf.append('['+d['name']+']\n')
+            newconf.append('    path = '+d['path']+'\n')
+            error = False
+            if d['writeable']:
+                newconf.append('    writeable = yes\n')
+            elif not d['writeable']:
+                newconf.append('    writeable = no\n')
+            else:
+                error = True
+            if d['teachers'] and d['students']:
+                # no new line needed, but config is valid
+                pass
+            elif d['teachers'] and not d['students']:
+                newconf.append('    valid users = @teachers\n')
+            elif not (d['teachers']) and (d['students']):
+                newconf.append('    valid users = @students\n')
+            else:
+                error = True
+            if error:
+                raise InputError('not all values are declared, please reconfigure samba-folders (might be in inconsistent state at the moment)')
+        newconf.append("# end ExchangeFolders; DON'T CHANGE MANUALLY!\n")
+        # delete old config:
         old = open(conffile, 'r')
         lines = old.readlines()
         old.close()
         new = open(conffile, 'w')
         found = False
-        # delete old config:
         for line in lines:
             if line == "# begin ExchangeFolders; DON'T CHANGE MANUALLY!\n":
                 found = True
@@ -48,35 +95,20 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                 new.write(line)
             if line == "# end ExchangeFolders; DON'T CHANGE MANUALLY!\n":
                 found = False
-        # Write new Config
-        new.write("# begin ExchangeFolders; DON'T CHANGE MANUALLY!\n")
-        j = json.loads(data)
-        for d in j:
-            new.write('['+d['name']+']\n')
-            new.write('    path = '+d['path']+'\n')
-            error = False
-            if d['writeable']:
-                new.write('    writeable = yes\n')
-            elif not d['writeable']:
-                new.write('    writeable = no\n')
-            else:
-                error = True
-            if d['teachers'] and d['students']:
-                # no new line needed, but config is valid
-                pass
-            elif d['teachers'] and not d['students']:
-                new.write('    valid users = @teachers\n')
-            elif not (d['teachers']) and (d['students']):
-                new.write('    valid users = @students\n')
-            else:
-                error = True
-            if error:
-                raise InputError('not all values are declared, please reconfigure samba-folders (might be in inconsistent state at the moment)')
-        new.write("# end ExchangeFolders; DON'T CHANGE MANUALLY!\n")
+        # write new stuff:
+        new.writelines(newconf)
         new.close()
+        # write to bakfile:
+        bak = open(bakfile, 'w')
+        bak.truncate()
+        bak.writelines(newconf)
+        bak.close()
+        # reload samba:
         os.system('service smbd reload')
         #os.system('service nmbd force-reload')
 
+if os.path.isfile(bakfile):
+    includeBackup()
 socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer(('', 8000), Handler)
 httpd.serve_forever()
