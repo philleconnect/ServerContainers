b192038c9c0258336c837af894842c5a7df52524
polarwinkel <it@polarwinkel.de>
2018-11-20 23:23:56 +0100

    updated volume names, included a script to change existing ones

--- dev/cleanup.sh
+++ dev/cleanup.sh
@@ -8,10 +8,10 @@ then
     docker rm pc_admin
     docker rm samba
     docker rm ldap
-    docker rmi servercontainers_php_ldap_admin
-    docker rmi servercontainers_pc_admin
-    docker rmi servercontainers_samba
-    docker rmi servercontainers_ldap
+    docker rmi philleconnect_php_ldap_admin
+    docker rmi philleconnect_pc_admin
+    docker rmi philleconnect_samba
+    docker rmi philleconnect_ldap
     docker volume rm servercontainers_philleconnect_admin_config
     docker volume rm servercontainers_philleconnect_admin_mysql
     docker volume rm servercontainers_philleconnect_ldap_db
