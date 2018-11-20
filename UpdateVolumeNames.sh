3d37a91beab73d43436966b977222fd392575f8b
polarwinkel <it@polarwinkel.de>
2018-11-20 23:38:51 +0100

    removed unused UpdateVolumeNames

--- UpdateVolumeNames.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo mv /var/lib/docker/volumes/servercontainers_philleconnect_admin_config /var/lib/docker/volumes/philleconnect_admin_config
-sudo mv /var/lib/docker/volumes/servercontainers_philleconnect_admin_mysql /var/lib/docker/volumes/philleconnect_admin_db
-sudo mv /var/lib/docker/volumes/servercontainers_philleconnect_admin_db /var/lib/docker/volumes/philleconnect_admin_db
