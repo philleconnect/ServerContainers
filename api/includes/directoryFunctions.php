4160231c30f6596c407b21802af461c35e2d94d8
polarwinkel <it@polarwinkel.de>
2018-12-06 16:59:23 +0100

    set homedirs back to 777 on fixHomedir: access-Server like samba shall do the auth stuff

--- api/includes/directoryFunctions.php
+++ api/includes/directoryFunctions.php
@@ -27,7 +27,7 @@
             mkdir($dir);
         }
         //chmod($dir,0777);
-        shell_exec('sudo chmod 0751 '.$dir);
+        shell_exec('sudo chmod 0777 '.$dir);
         shell_exec('sudo chown '.$uid.':'.$gid.' '.$dir);
         if (is_writeable($dir)) {
             return true;
